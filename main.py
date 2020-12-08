5cb1797a5e685a760fcac04e6955410279f40a22
bigjack <1465328859@qq.com>
2020-12-08 22:49:07 +0800

    创建分支3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('分支1')
     print('分支2')
+    print('分支3')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
